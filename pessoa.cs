b463dfdc5496cdade16f66a876f7f04b8a51792d
rapassos <rapassos@gmail.com>
2024-07-23 01:07:17 -0300

    Exemplo de criação de classe abstrata extendida de outra classe abstrata

---
+++ pessoa.cs
@@ -0,0 +1,27 @@
+using CSharp;
+
+Gerente g = new()
+{
+    Nome = "João"
+};
+g.MostraNome();
+g.MostraFuncao();
+
+
+public abstract class AbsPessoa{
+    public string? Nome { get; set; }
+    public abstract void MostraNome();
+}
+
+public abstract class Funcionario : AbsPessoa{
+    public override void MostraNome()
+    {
+        Console.WriteLine($"Meu nome é {Nome}.");
+    }
+}
+
+public class Gerente : Funcionario{
+    public void MostraFuncao(){
+        Console.WriteLine($"Minha função é Gerente");
+    }
+}
